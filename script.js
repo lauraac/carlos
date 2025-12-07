594606fe17aa73de5d0b116fabe121ace4c71fb5
lauraac <laura-a-c@hotmail.com>
2025-12-06 23:19:42 -0600

    chore

--- script.js
+++ script.js
@@ -44,3 +44,15 @@ function updateCountdown() {
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
+// OPCIONAL: por si en el futuro usas un <button> en lugar de <a>
+const btnMapa = document.getElementById("btnMapa");
+if (btnMapa) {
+  btnMapa.addEventListener("click", () => {
+    // Si quitas el href del <a>, esto mantiene la funcionalidad.
+    window.open(
+      "https://www.google.com/maps/@20.934224,-89.5683757,3a,90y,115.7h,66.33t/data=!3m7!1e1!3m5!1sxfxPQWvOL-qJA2C0d0ZZGA!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D23.672873999999993%26panoid%3DxfxPQWvOL-qJA2C0d0ZZGA%26yaw%3D115.699295!7i16384!8i8192",
+      "_blank"
+    );
+  });
+}
